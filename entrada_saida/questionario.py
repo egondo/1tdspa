6d0a7bf5dca23d263d8db27c1bdfad56a9b252f3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 09:59:46 -0300

    Entrada e saida

---
+++ entrada_saida/questionario.py
@@ -0,0 +1,14 @@
+nome = input("Nome? ")
+idade = input("Idade?")
+curso_superior = input("Curso superior? Qual?")
+escolha_TI = input("Por que escolheu TI?")
+area_atuacao = input("Dentro de TI, em que area gostaria de atuar?")
+hobbies = input("Quais são os seus hobbies?")
+
+
+print("Nome ", nome)
+print("Idade", idade)
+print("Curso superior", curso_superior)
+print("Escolha TI", escolha_TI)
+print("Area de atuacao", area_atuacao)
+print("Hobbies", hobbies)
