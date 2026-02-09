a8d9f8a190842501fcd0613ffc9168b79482cdd6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-09 10:00:07 -0300

    Jogo de Baralho

---
+++ jogo_baralho/jogo_baralho_inocente.py
@@ -0,0 +1,22 @@
+import baralho
+
+maco = baralho.cria('Normal')
+baralho.embaralha2(maco)
+
+pts_jog1 = 0
+pts_jog2 = 0
+
+for i in range(26):
+    c1 = baralho.compra(maco)
+    c2 = baralho.compra(maco)
+
+    print(baralho.gera_string_carta(c1))
+    print(baralho.gera_string_carta(c2))
+
+    if c1[0] < c2[0]:
+        pts_jog1 = pts_jog1 + 1
+    else:
+        pts_jog2 = pts_jog2 + 1
+
+print(f"Jogador 1: {pts_jog1}")
+print(f"Jogador 2: {pts_jog2}")
