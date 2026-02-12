f834b427ce445b35cc159a3815c95f6202c0e0c9
Eduardo Gondo <egondo@gmail.com>
2026-02-12 10:00:16 -0300

    Baralho e carta OO

---
+++ jogo_baralho/card.py
@@ -0,0 +1,8 @@
+class Card:
+    def __init__(self, valor: int, naipe: str):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __str__(self):
+        return f"{self.valor}{self.naipe}"
+
