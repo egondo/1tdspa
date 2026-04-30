02cbbf48b034b8dd9911afc273bd31bdf183b1de
Eduardo Gondo <egondo@gmail.com>
2026-04-30 09:08:12 -0300

    Adicionando na API a politica que ignora o CORS

--- sistema_paciente/hospital_curacerta/app.py
+++ sistema_paciente/hospital_curacerta/app.py
@@ -2,11 +2,14 @@
 #Funciona como um adaptador entre a interface e a camada de ngócio da aplicação (adapter)
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import negocio
 
 app = Flask("API Cura Certa")
+CORS(app)
 
 @app.route("/hospital/senha/<cpf>", methods=['GET'])
+@cross_origin()
 def gera_senha(cpf: str):
     senha = negocio.gerando_senha(cpf)
     info = {"numero": senha, "cpf": cpf}
